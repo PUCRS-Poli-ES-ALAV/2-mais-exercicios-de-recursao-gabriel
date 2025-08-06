65792141b7cb597f2fdc14201d0959957a279330
Gabriel-Fanti <138098905+Gabriel-Fanti@users.noreply.github.com>
2025-08-06 23:28:03 +0000

    commit dos guri

---
+++ exercicios.java
@@ -0,0 +1,61 @@
+public class exercicios{
+
+    public static void main (String[]args){
+        
+        System.out.println("Testando:");
+        System.out.println("Resultado: "+isPal("abba"));
+
+    }
+
+    public static int ex1(int n){
+        if(n<0){
+            throw new IllegalArgumentException("O número deve ser maior que 0!");
+        }
+        if(n == 0 || n == 1){
+            return 1;
+        }
+        return n*=ex1(n-1);
+    }
+
+    public static int ex2(int n) {
+        if(n == 0) {
+            return 0;
+        }
+        if (n > 0) {
+            return n+ex2(n-1);
+        }
+        return n+ex2(n+1);
+    }
+
+    public static int ex3(int n){
+        if(n <= 0){
+            throw new IllegalArgumentException("O número não pode ser menor ou igual a 0!");
+        }
+        if(n == 1 || n == 2){
+            return 1;
+        }
+        return n=ex3(n-1)+ex3(n-2);
+    }
+
+    public static int ex4(int j, int k) {
+        if(j == k) {
+            return k;
+        }
+        if (j > k) {
+            return k+ex4(j, k+1);
+        }
+        return j+ex4(j+1, k);
+    }
+
+    public static boolean isPal(String s){
+        if(s.lenght() == 1 || s.charAt(s.length()-1) == s.charAt(s.length()-2)){
+            return true;
+        }
+        if(s.charAt(0) != s.charAt(s.length()-1)){
+            return false;
+        }
+
+        return isPal(s.substring(1,s.length()-2));    
+    }
+
+}
