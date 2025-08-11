29fedf295ab6112ebd02b9e3cb30eb4ea325625d
Eduardo Tomacheski <tomacheski2001@gmail.com>
2025-08-11 23:33:50 +0000

    create permutations

--- exercicios.java
+++ exercicios.java
@@ -1,10 +1,9 @@
 
 import java.util.ArrayList;
 
+public class exercicios {
+    public static void main(String[] args) {
 
-public class exercicios{    
-    public static void main (String[]args){
-        
         // System.out.println("Testando:");
         // System.out.println("Resultado: "+intToBinary(157));
 
@@ -16,85 +15,85 @@ public class exercicios{
 
         // System.err.println("Resultado soma array: "+ somaArray(array));
 
-        System.out.println("Resultado: "+nroDigit(17989));
+        System.out.println("Resultado: " + nroDigit(17989));
 
     }
 
-    public static int ex1(int n){
-        if(n<0){
+    public static int ex1(int n) {
+        if (n < 0) {
             throw new IllegalArgumentException("O número deve ser maior que 0!");
         }
-        if(n == 0 || n == 1){
+        if (n == 0 || n == 1) {
             return 1;
         }
-        return n*=ex1(n-1);
+        return n *= ex1(n - 1);
     }
 
     public static int ex2(int n) {
-        if(n == 0) {
+        if (n == 0) {
             return 0;
         }
         if (n > 0) {
-            return n+ex2(n-1);
+            return n + ex2(n - 1);
         }
-        return n+ex2(n+1);
+        return n + ex2(n + 1);
     }
 
-    public static int ex3(int n){
-        if(n <= 0){
+    public static int ex3(int n) {
+        if (n <= 0) {
             throw new IllegalArgumentException("O número não pode ser menor ou igual a 0!");
         }
-        if(n == 1 || n == 2){
+        if (n == 1 || n == 2) {
             return 1;
         }
-        return n=ex3(n-1)+ex3(n-2);
+        return n = ex3(n - 1) + ex3(n - 2);
     }
 
     public static int ex4(int j, int k) {
-        if(j == k) {
+        if (j == k) {
             return k;
         }
         if (j > k) {
-            return k+ex4(j, k+1);
+            return k + ex4(j, k + 1);
         }
-        return j+ex4(j+1, k);
+        return j + ex4(j + 1, k);
     }
 
-    public static boolean isPal(String s){
-        if(s.charAt(0) != s.charAt(s.length()-1)){
+    public static boolean isPal(String s) {
+        if (s.charAt(0) != s.charAt(s.length() - 1)) {
             return false;
         }
-        if(s.length() == 1){
+        if (s.length() == 1) {
             return true;
         }
-        isPal(s.substring(1,s.length()-2));
-        return true;    
+        isPal(s.substring(1, s.length() - 2));
+        return true;
     }
 
-    public static String intToBinary (int n) {
-        if(n == 0) {
+    public static String intToBinary(int n) {
+        if (n == 0) {
             return "0";
         }
-        if(n == 1) {
+        if (n == 1) {
             return "1";
         }
         String aux = Integer.toString(n % 2);
-        return intToBinary(n/2) + aux;
+        return intToBinary(n / 2) + aux;
     }
 
-    public static int somaArray (ArrayList<Integer> array) {
-        if (array.size() == 0){
+    public static int somaArray(ArrayList<Integer> array) {
+        if (array.size() == 0) {
             return 0;
         }
-        int aux = array.remove(array.size()-1);
+        int aux = array.remove(array.size() - 1);
         return aux + somaArray(array);
     }
 
-    public static int nroDigit (int n) {
+    public static int nroDigit(int n) {
         if (n < 10) {
             return 1;
         }
-        return 1 + nroDigit(n/10);
+        return 1 + nroDigit(n / 10);
     }
 
     public static boolean findSubStr(String str, String match) {
@@ -110,16 +109,39 @@ public class exercicios{
         return findSubStr(str.substring(1), match);
     }
 
-    public static int findBiggest(ArrayList<Integer> ar){
-        if(ar.size()==1){
+    public static int findBiggest(ArrayList<Integer> ar) {
+        if (ar.size() == 1) {
             return ar.get(0);
         }
-        int aux = ar.remove(ar.size()-1);
+        int aux = ar.remove(ar.size() - 1);
         int aux2 = findBiggest(ar);
         if (aux > aux2) {
             return aux;
         }
         return aux2;
+    }
+
+    public static ArrayList<String> permutations(String s) {
+        ArrayList<String> resultado = new ArrayList<>();
+
+        if (s.length() == 1) {
+            resultado.add(s);
+            return resultado;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            String restante = s.substring(0, i) + s.substring(i + 1);
+
+            ArrayList<String> permRestante = permutations(restante);
+
+            for (String perm : permRestante) {
+                resultado.add(c + perm);
+            }
+        }
+
+        return resultado;
     }
 
 }
